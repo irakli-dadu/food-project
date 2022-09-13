dd6ee9cd86d229022b6df34aeecad96584d64798
irakli-dadu <lipsowow@gmail.com>
2022-09-13 23:30:09 +0400

    works done brazzer
    yooo

--- js/script.js
+++ js/script.js
@@ -60,6 +60,59 @@ window.addEventListener("scroll", function () {
 });
 
 
-// cta
+// cta - validation-form
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+
+    const username = document.querySelector("[name = 'username']").value.trim()
+    const email = document.querySelector("[name = 'email']").value.trim()
+    const password = document.querySelector("[name = 'mypassword']").value.trim()
+    const confirmPas = document.querySelector("[name = 'confpassword']").value.trim()
+
+
+    const errors = []
+
+    if(username === '') {
+        errors.push("Username can't be blank")
+    }
+    if(email === '') {
+        errors.push("Email can't be blank")
+    }
+    if(password === '') {
+        errors.push("password can't be blank")
+    }
+    if(password !== confirmPas ) {
+        errors.push("Passwords must match!")
+    }
+
+    if(errors.length > 0) {
+        for(let i = 0; i < errors.length; i++) {
+            Toastify({
+                text: errors[i],
+                className: "error-info-toast",
+                duration: 4000,
+                gravity: "top",
+                position: "center",
+                style: {
+                    background: "#e26465",
+                }
+            }).showToast()
+        }
+    }else {
+        Toastify({
+            text: "Success!",
+            className: "success-info-toast",
+            duration: 4000,
+            gravity: "top",
+            position: "center",
+            style: {
+                background: "#cedb73"
+            }
+        }).showToast()
+    }
+})
 
 
